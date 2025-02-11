d1a9ce00fedd67dc86e40018a8106b6736593883
V1ntester <valentin_n_valen@mail.ru>
2025-02-11 16:26:49 +0300

    Format code

--- MyMath/MyMath.cpp
+++ MyMath/MyMath.cpp
@@ -1,7 +1,5 @@
 #include "MyMath.h"
 
 namespace MyMath {
-    int Sum(int firstValue, int secondValue) {
-        return firstValue + secondValue;
-    }
-}
+int Sum(int firstValue, int secondValue) { return firstValue + secondValue; }
+}  // namespace MyMath
