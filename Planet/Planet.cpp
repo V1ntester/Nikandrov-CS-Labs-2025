849cd072fa107059b3053c08224f6f596f8f01fd
V1ntester <valentin_n_valen@mail.ru>
2025-02-12 23:15:02 +0300

    temp

--- Planet/Planet.cpp
+++ Planet/Planet.cpp
@@ -1,16 +1,52 @@
 #include "Planet.h"
 #include <cstddef>
 #include <cstring>
+#include <fstream>
 #include <iostream>
 
 using std::size_t;
 
-Planet::Planet(const char* name, size_t diameter, size_t population, size_t satellitesCount)
-    : diameter(diameter), population(population), satellitesCount(satellitesCount) {
-    size_t nameLength = strlen(name) + 1;
-    this->name = new char[nameLength];
+namespace {
+const size_t kBufferSize = 256;
+}
 
-    strlcpy(this->name, name, nameLength);
+std::ifstream& operator>>(std::ifstream& in, Planet planet) {
+    char name[kBufferSize]{'\0'};
+    size_t diameter = 0;
+    size_t population = 0;
+    size_t satellitesCount = 0;
+
+    in.getline(name, kBufferSize - 1, ' ');
+    in >> diameter >> population >> satellitesCount;
+
+    planet.SetName(name);
+    planet.SetDiameter(diameter);
+    planet.SetPopulation(population);
+    planet.SetSatellitesCount(satellitesCount);
+    return in;
+}
+
+std::ofstream& operator<<(std::ofstream& out, Planet planet) {
+    out << planet.GetName() << ' ' << planet.GetDiameter() << ' ' << planet.GetPopulation() << ' ' << planet.GetSatellitesCount() << '\n';
+    return out;
+}
+
+void Planet::InitName(const char* name, size_t nameLength) {
+    this->nameLength = nameLength;
+    this->name = new char[this->nameLength];
+    strlcpy(this->name, name, this->nameLength);
+}
+
+Planet::Planet(const char* name, size_t diameter, size_t population, size_t satellitesCount)
+    : id(0), diameter(diameter), population(population), satellitesCount(satellitesCount) {
+    size_t nameLength = strlen(name) + 1;
+    this->InitName(name, nameLength);
+}
+
+Planet::Planet(size_t id, const char* name, size_t diameter, size_t population, size_t satellitesCount)
+    : id(id), diameter(diameter), population(population), satellitesCount(satellitesCount) {
+    size_t nameLength = strlen(name) + 1;
+    this->InitName(name, nameLength);
 }
 
 Planet::~Planet() {
@@ -33,8 +69,14 @@ size_t Planet::GetSatellitesCount() {
     return this->satellitesCount;
 }
 
-void Planet::SetName(char* name) {
-    this->name = name;
+void Planet::SetName(const char* name) {
+    size_t newNameLength = strlen(name) + 1;
+
+    if (!this->nameLength && this->nameLength != newNameLength) {
+        delete[] this->name;
+    }
+
+    this->InitName(name, newNameLength);
 }
 
 void Planet::SetDiameter(size_t diameter) {
--- Planet/Planet.h
+++ Planet/Planet.h
@@ -5,20 +5,26 @@
 
 class Planet {
  private:
+    size_t id = 0;
     char* name = nullptr;
+    size_t nameLength = 0;
     size_t diameter = 0;
     size_t population = 0;
     size_t satellitesCount = 0;
 
+    void InitName(const char* name, size_t nameLength);
+
  public:
     Planet(const char* name, size_t diameter, size_t population, size_t satellitesCount);
+    Planet(size_t id, const char* name, size_t diameter, size_t population, size_t satellitesCount);
     ~Planet();
 
+    size_t GetId();
     char* GetName();
     size_t GetDiameter();
     size_t GetPopulation();
     size_t GetSatellitesCount();
-    void SetName(char* name);
+    void SetName(const char* name);
     void SetDiameter(size_t diameter);
     void SetPopulation(size_t population);
     void SetSatellitesCount(size_t satellitesCount);
