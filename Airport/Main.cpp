19e14e5955ad42504a27e9d3949e88096bf8400c
V1ntester <valentin_n_valen@mail.ru>
2025-03-12 12:49:25 +0300

    Refactor code

--- Airport/Main.cpp
+++ Airport/Main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int main(int argc, char** argv) {
+int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
     std::cout << "App for library testing\n";
 
     return 0;
